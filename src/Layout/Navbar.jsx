02c63f4ab3420893b1209ff9cb060e4234510f39
Shubrotto Kumar Hawlader <skshubroto@gmail.com>
2024-03-29 22:55:32 +0600

    humburger menu feature add

--- src/Layout/Navbar.jsx
+++ src/Layout/Navbar.jsx
@@ -1,28 +1,27 @@
 import "./navbar.css";
-import { Menu } from "@material-ui/icons";
+import { CloseOutlined, Menu } from "@material-ui/icons";
+import { useState } from "react";
 import { FaSmile } from "react-icons/fa";
 
 const menu = [
   { id: 1, navTitle: "Home" },
   { id: 2, navTitle: "Blog" },
   { id: 3, navTitle: "Portfolio" },
-  { id: 4, navTitle: "Contact" },
-  { id: 5, navTitle: "About" },
+  { id: 4, navTitle: "Gallery" },
+  { id: 5, navTitle: "Contact" },
+  { id: 6, navTitle: "About" },
 ];
 
 const Navbar = () => {
+  const [IsClicked, setIsClicked] = useState(false);
   return (
     <div className="nav_container">
       <div className="nav_wrapper">
-        <h1 className="title">
+        <a className="title" href="#">
           Alw<span className="title_center">a</span>ys
           <FaSmile className="logo_icon" />
-        </h1>
-        <Menu
-          className="menu_icon"
-          style={{ display: "none", width: "30px" }}
-        />
-        <ul className="nav_lists">
+        </a>
+        <ul className={IsClicked ? "nav_lists_md_sm" : "nav_lists"}>
           {menu.map((item) => (
             <li key={item.id} className="nav_item">
               <a>{item.navTitle}</a>
@@ -30,6 +29,19 @@ const Navbar = () => {
           ))}
         </ul>
       </div>
+
+      {IsClicked === true ? (
+        <CloseOutlined
+          className="closed_icon"
+          onClick={() => setIsClicked(!IsClicked)}
+        />
+      ) : (
+        <Menu
+          className="menu_icon"
+          style={{ display: "none", width: "30px" }}
+          onClick={() => setIsClicked(!IsClicked)}
+        />
+      )}
     </div>
   );
 };
